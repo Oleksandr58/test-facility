eaa1cbe27e3737aef4a2ef979045614a85c300d1
Oleksandr58 <33155100+Oleksandr58@users.noreply.github.com>
2019-01-14 02:16:04 +0200

    Slider

---
+++ app/js/layout/slider.js
@@ -0,0 +1,11 @@
+$('.js-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 5000,
+    speed: 500,
+    prevArrow: '.js-slider-prev',
+    nextArrow: '.js-slider-next',
+    zIndex: 0
+});
+
